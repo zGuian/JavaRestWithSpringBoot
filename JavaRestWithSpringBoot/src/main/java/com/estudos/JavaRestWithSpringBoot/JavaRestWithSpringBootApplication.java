ad1e1235acc0dfc33517318e8e846c2ee23f1283
Guian Rocha <guian.rocha@t-systems.com>
2024-10-29 14:22:38 -0300

    Arquivos do spring

---
+++ JavaRestWithSpringBoot/src/main/java/com/estudos/JavaRestWithSpringBoot/JavaRestWithSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.estudos.JavaRestWithSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaRestWithSpringBootApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JavaRestWithSpringBootApplication.class, args);
+	}
+}
---
+++ JavaRestWithSpringBoot/src/test/java/com/estudos/JavaRestWithSpringBoot/JavaRestWithSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudos.JavaRestWithSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaRestWithSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
