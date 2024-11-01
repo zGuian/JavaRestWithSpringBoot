556641abc520938558b319520260d2a3b94f220d
zGuian <guianmarcos32@gmail.com>
2024-11-01 10:16:29 -0300

    Adicionado configuração de cors

--- JavaRestWithSpringBoot/src/main/java/com/estudos/JavaRestWithSpringBoot/Config/WebConfig.java
+++ JavaRestWithSpringBoot/src/main/java/com/estudos/JavaRestWithSpringBoot/Config/WebConfig.java
@@ -1,12 +1,17 @@
 package com.estudos.JavaRestWithSpringBoot.Config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.OriginPatterns:default}")
+    private String corsOriginPatterns = "";
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
@@ -26,5 +31,14 @@ public class WebConfig implements WebMvcConfigurer {
             .defaultContentType(MediaType.APPLICATION_JSON)
                 .mediaType("json", MediaType.APPLICATION_JSON)
                 .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        var allowedOrigins = corsOriginPatterns.split(",");
+        registry.addMapping("/**")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins(allowedOrigins)
+                .allowCredentials(true);
     }
 }
